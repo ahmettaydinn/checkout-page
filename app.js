9e04fe43d897591af715faf17bcff06617c824ee
Ahmet <birceste@gmail.com>
2022-09-25 23:42:14 +0300

    project is being remade

--- app.js
+++ app.js
@@ -1,46 +1,15 @@
-let shoesMinus = document.getElementById("shoes-minus");
-let shoesPlus = document.getElementById("shoes-plus");
-let backpackMinus = document.getElementById("backpack-minus");
-let backpackPlus = document.getElementById("backpack-plus");
-let total = document.getElementById("spanTwo");
+const taxRate = 0.18;
+const shippingPrice = 15;
+const shippingFreePrice = 300;
 
-let shoesCounter = document.getElementById("shoes-counter");
-let backpackCounter = document.getElementById("backpack-counter");
-let shoesCount = 0;
-let backpackCount = 0;
-let totalCount = 0;
+window.addEventListener("load", () => {
+  // set items to LocalStorage
+  localStorage.setItem("taxRate", taxRate);
+  localStorage.setItem("shippingPrice", shippingPrice);
+  localStorage.setItem("shippingFreePrice", shippingFreePrice);
 
-shoesPlus.addEventListener("click", function () {
-  shoesCount += 1;
-  shoesCounter.textContent = shoesCount;
-  totalCount += 74;
-  total.textContent = `$${totalCount}`;
+  // set items to SessionStorage
+  sessionStorage.setItem("taxRate", taxRate);
+  sessionStorage.setItem("shippingPrice", shippingPrice);
+  sessionStorage.setItem("shippingFreePrice", shippingFreePrice);
 });
-
-shoesMinus.addEventListener("click", function () {
-  shoesCount -= 1;
-  shoesCounter.textContent = shoesCount;
-  totalCount -= 74;
-  total.textContent = `$${totalCount}`;
-});
-
-backpackPlus.addEventListener("click", function () {
-  backpackCount += 1;
-  backpackCounter.textContent = backpackCount;
-  totalCount += 54;
-  total.textContent = `$${totalCount}`;
-});
-
-backpackMinus.addEventListener("click", function () {
-  backpackCount -= 1;
-  backpackCounter.textContent = backpackCount;
-  totalCount -= 54;
-  total.textContent = `$${totalCount}`;
-});
-
-
-
-
-
-
-
